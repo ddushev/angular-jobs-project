c703a5c14c5fec15d7eb75137d1673279694fa70
Daniel Dushev <danieldyshew@gmail.com>
2024-02-06 22:14:27 +0200

    components. add components and routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { JobListComponent } from './job-list/job-list.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { JobDetailsComponent } from './job-details/job-details.component';
+import { CreateJobComponent } from './create-job/create-job.component';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
 
 export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'job-listings', component: JobListComponent},
+  {path: 'job-details', component: JobDetailsComponent},
+  {path: 'create-job', component: CreateJobComponent},
   {path: '**', component: NotFoundComponent},
 ];
