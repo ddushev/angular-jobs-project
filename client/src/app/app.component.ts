8020cd7fe79b5405ec40b41d2054e882eda04bca
Daniel Dushev <danieldyshew@gmail.com>
2024-02-06 13:46:21 +0200

    add basic structure

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { JobListComponent } from './job-list/job-list.component';
+import { SearchComponent } from './core/search/search.component';
+import { AboutComponent } from './about/about.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { BackToTopComponent } from './core/back-to-top/back-to-top.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    AboutComponent,
+    CategoriesComponent,
+    BackToTopComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { JobListComponent } from './job-list/job-list.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'job-listings', component: JobListComponent},
+  {path: 'about', component: AboutComponent},
+];
