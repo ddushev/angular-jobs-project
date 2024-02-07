0dc673b21a8edcbb107cf1f722e1d885dff8464b
Daniel Dushev <danieldyshew@gmail.com>
2024-02-07 23:40:04 +0200

    auth.interceptor. add accessToken to request if exist

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { JobListComponent } from './components/job-list/job-list.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { JobDetailsComponent } from './components/job-details/job-details.component';
 import { CreateJobComponent } from './components/create-job/create-job.component';
-import { LoginComponent } from './components/account/login/login.component';
-import { RegisterComponent } from './components/account/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
--- client/src/app/interceptors/auth.interceptor.ts
+++ client/src/app/interceptors/auth.interceptor.ts
@@ -1,13 +1,26 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { Store } from '@ngrx/store';
+import { inject } from '@angular/core';
+import { authState } from '../state/auth.selector';
+import { Observable, switchMap, take } from 'rxjs';
+import { AuthState } from '../state/auth.state';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const store = inject(Store);
   const apiURL = environment.apiURL
-  if (req.url.startsWith(apiURL)) {
-    req = req.clone({
-      setHeaders: {
+  const authState$: Observable<AuthState> = store.select(authState);
+  return authState$.pipe(
+    take(1),
+    switchMap(authState => {
+      if (req.url.startsWith(apiURL) && authState.user && authState.user.accessToken) {
+        req = req.clone({
+          setHeaders: {
+            'X-Authorization': authState.user?.accessToken
+          }
+        })
       }
+      return next(req);
     })
-  }
-  return next(req);
+  );
 };
