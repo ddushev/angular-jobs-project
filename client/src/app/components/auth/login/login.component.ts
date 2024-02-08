1259ec69dee4059db6e9400ca80e2e84591d64db
Daniel Dushev <danieldyshew@gmail.com>
2024-02-08 11:06:44 +0200

    register.comp. add template driven form handling

--- client/src/app/components/auth/login/login.component.ts
+++ client/src/app/components/auth/login/login.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Store } from '@ngrx/store';
-import { AuthApiActions } from '../../../state/auth.actions';
 import { Router } from '@angular/router';
+import { AuthApiActions } from '../../../state/auth.actions';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, AsyncPipe, JsonPipe],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
--- client/src/app/components/auth/register/register.component.ts
+++ client/src/app/components/auth/register/register.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
+  constructor(private store: Store, private router: Router) { }
+
+  handleRegister(form: NgForm) {
+    if (form.invalid) {
+      form.reset();
+      return;
+    }
+    console.log(form.value);
+    // this.router.navigate(['/job-listings'])
+    // form.reset();
+  }
 }
