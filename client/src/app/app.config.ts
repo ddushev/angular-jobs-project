ca697b388ff4b94dd2027316a7124ef7197f2ddf
Daniel Dushev <danieldyshew@gmail.com>
2024-02-11 15:41:56 +0200

    authentication.interceptor. rename and move

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,17 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptors/auth.interceptor';
+import { authenticationInterceptor } from './interceptors/authentication/authentication.interceptor';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideEffects } from '@ngrx/effects';
 import { AuthEffects } from './state/auth.effects';
 import { authReducer } from './state/auth.reducer';
+import { authorizationInterceptor } from './interceptors/authorization/authorization.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([authenticationInterceptor, authorizationInterceptor])),
     provideStore({authState: authReducer}),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
     provideEffects(AuthEffects),
--- client/src/app/interceptors/authentication/authentication.interceptor.ts
+++ client/src/app/interceptors/authentication/authentication.interceptor.ts
@@ -1,12 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { Store } from '@ngrx/store';
 import { inject } from '@angular/core';
-import { authState } from '../state/auth.selector';
+import { authState } from '../../state/auth.selector';
 import { Observable, switchMap, take } from 'rxjs';
-import { IAuthState } from '../state/auth.state';
+import { IAuthState } from '../../state/auth.state';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
+export const authenticationInterceptor: HttpInterceptorFn = (req, next) => {
   const store = inject(Store);
   const apiURL = environment.apiURL
   const authState$: Observable<IAuthState> = store.select(authState);
