05a1d4a63fa35bc48b1c2c9dc831d9b53751f390
Daniel Dushev <danieldyshew@gmail.com>
2024-02-13 12:29:06 +0200

    job-details. add buttons for owners

--- client/src/app/components/job-details/job-details.component.ts
+++ client/src/app/components/job-details/job-details.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { IJob } from '../../types/job';
 import { ActivatedRoute } from '@angular/router';
-import { DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IAuthState } from '../../state/auth.state';
+import { authState } from '../../state/auth.selector';
 
 @Component({
   selector: 'app-job-details',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, AsyncPipe],
   templateUrl: './job-details.component.html',
   styleUrl: './job-details.component.scss'
 })
 export class JobDetailsComponent implements OnInit {
+  authState$: Observable<IAuthState> = this.store.select(authState);
   job!: IJob
 
-  constructor(private activeRoute: ActivatedRoute) {}
+  constructor(private store: Store, private activeRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.activeRoute.data.subscribe((data) => {
